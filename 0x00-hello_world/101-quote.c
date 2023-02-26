0532ab29ac0b9371a2f4acc22f06590ca7711661
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-02-26 13:36:52 -0800

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - A program that prints
+ *
+ * Return: 1 (unsuccessful)
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
