f80858bd19ddc124fd9bac64c42da4447ec7337b
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-02-27 11:40:15 -0800

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,24 +5,22 @@
  *
  * Return: Always 0 (Succes)
  */
-
 int main(void)
 {
         int n;
-        int i;
-
-        for (n = 0; n < 10; n++)
+	int i;
+	
+	for (n = 0; n < 10; n++)
         {
-                for (i = 0; i < 10; i++)
+		for (i = 0; i < 10; i++)
 		{
 			if (n == i)
 				continue;
 			putchar((n % 10) + '0');
 			putchar((i % 10)+ '0');
-
-		        putchar(',');
-                        putchar(' ');
-                }
+			putchar(',');
+			putchar(' ')
+		}
 	}
 
 	putchar ('\n');
