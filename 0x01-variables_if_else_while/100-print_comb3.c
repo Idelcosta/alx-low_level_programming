bdde12bfaa69b3159f0e906223d8e4a2be0dde2b
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-02-27 11:19:46 -0800

    tsk10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,26 +8,26 @@
 
 int main(void)
 {
-	int n;
-	int i = 0;
+        int n;
+        int i;
 
-	for (n = 0 ; n < 100 ; n++)
-	{
-		putchar(n / 10 + '0');
-		putchar(i + '0');
-		if (n < 99)
+        for (n = 0; n < 10; n++)
+        {
+                for (i = 0; i < 10; i++)
 		{
-			putchar(',');
-			putchar(32);
-		}
-		i++;
-		if (i > 9)
-		{
-			i = 0;
-		}
+			putchar((n % 10) + '0');
+			putchar((i % 10)+ '0');
+
+                        if (n == 9 && i == 9)
+			       continue;
+
+		        putchar(',');
+                        putchar(' ');
+                }
 	}
-	putchar('\n');
+
+	putchar ('\n');
+
 	return (0);
 }
-
 
