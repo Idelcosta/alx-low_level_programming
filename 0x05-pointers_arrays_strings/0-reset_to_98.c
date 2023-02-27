388fd98c0f0dfb3a59befd14ba5f83e73d397eac
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-02-27 14:42:36 -0800

    tasksk0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,9 @@
-/**
- * main - function that takes a pointer
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
+#include <stdio.h>
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+/**
+ * takes a pointer to an int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
