acf3dcb296feecd8c9191f2784eb12d81bda43cb
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-03-23 10:45:37 +0200

    a function that compares two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-
 	i = 0;
 	while (dest[i] != '\0')
 	{
@@ -14,9 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
