a4ef28d02c4c29bbfb46bdb98762d54a24a9fc70
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-08-30 04:03:22 +0300

    doubly linked

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,18 @@
-include "lists.h"
+#include "lists.h"
 /**
- * dlistint_len - show us the number of elements 
+ * dlistint_len - show us the number of elements in a DLL
  * @h: a pointer to the head of the DLL
- * Return: the number of elements
+ * Return: the number of elements in a DLL
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
-	count = 0;
-	if (h == NULL)
-		return (count);
-	while (h->prev != NULL)
-		h = h->prev;
-	while (h != NULL)
+	size_t num_of_elements;
+
+	num_of_elements = 0;
+	while (h)
 	{
-		count++;
 		h = h->next;
+		num_of_elements = num_of_elements + 1;
 	}
-	return (count);
+	return (num_of_elements);
 }
