f92bf46572154674f98439dae1676ce974051383
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-08-30 04:01:55 +0300

    doubly linked

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,13 +6,16 @@ include "lists.h"
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t num_of_elements;
-
-	num_of_elements = 0;
-	while (h)
+	int count;
+	count = 0;
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
 	{
+		count++;
 		h = h->next;
-		num_of_elements = num_of_elements + 1;
 	}
-	return (num_of_elements);
+	return (count);
 }
